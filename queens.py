d6c8d6b8e2c4720f25e2cb8057bfebb5ad75d924
Majid Joseph <majid.joseph7@gmail.com>
2021-02-19 19:06:09 -0500

    adding mutation and crossover

--- queens.py
+++ queens.py
@@ -4,15 +4,22 @@ import random
 
 NUMBER_OF_QUEENS = 8
 MAX_FITNESS_SCORE = int((NUMBER_OF_QUEENS - 1)*(NUMBER_OF_QUEENS)/2)
-POPULATION_SIZE = 20
+POPULATION_SIZE = 2
+MUTATION_RATE = 0.05
+MAX_GENERATIONS = 1
+
+class Board:
+    def __init__(self):
+        self.values = None
+        self.fitness = None
 
 def generate_board():
-    initial_population = []
+    board = []
 
     for i in range(NUMBER_OF_QUEENS):
-        initial_population.append(random.randint(0, NUMBER_OF_QUEENS - 1))
+        board.append(random.randint(0, NUMBER_OF_QUEENS - 1))
     
-    return initial_population
+    return board
 
 def fitness_function(board):
     score = MAX_FITNESS_SCORE
@@ -35,19 +42,50 @@ def fitness_function(board):
     return score
 
 def generate_population():
-    population = [generate_board() for i in range(POPULATION_SIZE)]
+
+    population = [Board() for i in range(POPULATION_SIZE)]
+
+    for board in population:
+        board.values = generate_board()
+        board.fitness = fitness_function(board.values)
+
+    population.sort(key=lambda x: x.fitness)
     return population
 
-def can_stop(population):
+def can_stop(population, generation):
     
-    if MAX_FITNESS_SCORE in [fitness_function(pos) for pos in population]:
+    if MAX_FITNESS_SCORE in [pos.fitness for pos in population]:
         return True
     
+    if generation > MAX_GENERATIONS:
+        return True
+
     return False
 
-def cross_over(population):
-    # Perform crossover here
-    return generate_population()
+def crossover(parent1, parent2):
+    child = Board()
+
+    child.values = parent1.values[:4]
+    child.values += parent2.values[4:]
+
+    child.fitness = fitness_function(child.values)
+    return child
+
+
+def mutate(board):
+    if random.uniform(0, 1) < MUTATION_RATE:
+        board.values[random.randint(0, NUMBER_OF_QUEENS - 1)] = random.randint(0, NUMBER_OF_QUEENS - 1)
+        board.fitness = fitness_function(board.values)
+    return board
+
+
+def genetic_algorithm(population):
+    newpopulation = []
+
+    for i in range(len(population)):
+	    print()
+
+    return newpopulation
 
 def main():
     if len(sys.argv) != 1:
@@ -56,12 +94,18 @@ def main():
 
     population = generate_population()
 
-    while not can_stop(population):
-        population = cross_over(population)
+    generation = 0
+
+    while not can_stop(population, generation):
+        population = genetic_algorithm(population)
+        generation += 1
+
+    print("Generation #:", generation, "\nSolutions:")
 
     for board in population:
-        if fitness_function(board) == MAX_FITNESS_SCORE:
-            print(board)
+        print(board.values, board.fitness)
+        if board.fitness == MAX_FITNESS_SCORE:
+            print(board.values)
 
 if __name__ == "__main__":
     main()
